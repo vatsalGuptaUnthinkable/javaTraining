7c87ca1048189c0a54ad030e2461c7d65dfcdb4d
vatsal <vatsal.gupta@unthinkable.co>
2021-11-23 10:57:53 +0530

    Changes in the initial project

--- SpringJdbcApi/src/main/java/com/unthinkable/SpringJdbcApi/App.java
+++ SpringJdbcApi/src/main/java/com/unthinkable/SpringJdbcApi/App.java
@@ -1,13 +1,22 @@
 package com.unthinkable.SpringJdbcApi;
 
-/**
- * Hello world!
- *
- */
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+
 public class App 
 {
-    public static void main( String[] args )
+
+	public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        ApplicationContext con = new ClassPathXmlApplicationContext("com/unthinkable/SpringJdbcApi/config.xml");
+        
+		JdbcTemplate js = (JdbcTemplate) con.getBean("jdT");
+		
+		String sql = "Insert into student(id,name,city) values(?,?,?)";
+		int result = js.update(sql,1,"vatsal","kanpur");
+		System.out.println("The number of rows executed are + =" + result);
     }
 }
