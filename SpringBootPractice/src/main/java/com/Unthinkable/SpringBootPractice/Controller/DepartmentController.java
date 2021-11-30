9137fb5ee69c36140b8f15784824d6cc87dd169f
vatsal <vatsal.gupta@unthinkable.co>
2021-11-30 17:47:07 +0530

    SpringBoot with Get and post Api

--- SpringBootPractice/src/main/java/com/Unthinkable/SpringBootPractice/Controller/DepartmentController.java
+++ SpringBootPractice/src/main/java/com/Unthinkable/SpringBootPractice/Controller/DepartmentController.java
@@ -1,8 +1,27 @@
 package com.Unthinkable.SpringBootPractice.Controller;
-
-
+import com.Unthinkable.SpringBootPractice.entity.Department;
+import com.Unthinkable.SpringBootPractice.service.DepartmentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class DepartmentController {
+
+    @Autowired
+    private DepartmentService departmentService;
+
+    @PostMapping("/department")
+    public Department saveDepartment(@RequestBody Department department){
+        return departmentService.saveDepartment(department);
+    }
+
+    @GetMapping("/department")
+    public List<Department> fetchDepartment(){
+        return departmentService.fetchDepartment();
+    }
 }
--- SpringBootPractice/src/main/java/com/Unthinkable/SpringBootPractice/service/DepartmentService.java
+++ SpringBootPractice/src/main/java/com/Unthinkable/SpringBootPractice/service/DepartmentService.java
@@ -1,4 +1,10 @@
 package com.Unthinkable.SpringBootPractice.service;
 
+import com.Unthinkable.SpringBootPractice.entity.Department;
+
+import java.util.List;
+
 public interface DepartmentService {
+    Department saveDepartment(Department department);
+    List<Department> fetchDepartment();
 }
