c422b55a71c5e88cf2d0a54d5570a69ca805c36f
vatsal <vatsal.gupta@unthinkable.co>
2021-12-06 12:35:46 +0530

    Repository layer added

--- SpringBootPractice/src/test/java/com/Unthinkable/SpringBootPractice/repository/DepartmentRepositoryTest.java
+++ SpringBootPractice/src/test/java/com/Unthinkable/SpringBootPractice/repository/DepartmentRepositoryTest.java
@@ -1,12 +1,40 @@
 package com.Unthinkable.SpringBootPractice.repository;
 
+import com.Unthinkable.SpringBootPractice.entity.Department;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@DataJpaTest
 class DepartmentRepositoryTest {
+
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Autowired
+    private DepartmentRepository departmentRepository;
+
 
     @BeforeEach
     void setUp() {
+        Department mockDepartment = Department.builder()
+                .departmentName("IT")
+                .departmentHead("Deepak")
+                .DepartmentId(1L)
+                .build();
+
+        Mockito.when(departmentRepository.findById(1L).get()).thenReturn(mockDepartment);
+    }
+
+    @Test
+    private void whenDepartmentExist_thenReturnDepartment() {
+        Department dept = departmentRepository.findById(1L).get();
+        assertEquals("IT", dept.getDepartmentName());
     }
 }
