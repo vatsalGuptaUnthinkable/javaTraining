a1af065221c7b8a139b6efdfdd1427feae514e25
vatsal <vatsal.gupta@unthinkable.co>
2021-12-06 13:19:43 +0530

    Controller layer

---
+++ SpringBootPractice/src/test/java/com/Unthinkable/SpringBootPractice/Controller/DepartmentControllerTest.java
@@ -0,0 +1,32 @@
+package com.Unthinkable.SpringBootPractice.Controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DepartmentControllerTest {
+
+    @Test
+    void saveDepartment() {
+    }
+
+    @Test
+    void fetchDepartment() {
+    }
+
+    @Test
+    void fetchDepartmentById() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+
+    @Test
+    void updateById() {
+    }
+
+    @Test
+    void fetchDataByName() {
+    }
+}
---
+++ SpringBootPractice/src/test/java/com/Unthinkable/SpringBootPractice/Controller/EmployeeControllerTest.java
@@ -0,0 +1,24 @@
+package com.Unthinkable.SpringBootPractice.Controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmployeeControllerTest {
+
+    @Test
+    void saveEmployee() {
+    }
+
+    @Test
+    void getAllEmployees() {
+    }
+
+    @Test
+    void getById() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+}
